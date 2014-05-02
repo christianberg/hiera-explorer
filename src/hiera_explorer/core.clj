c82767890f163903f7d0f7b9d04b2731d29000cd
Christian Berg <berg.christian@gmail.com>
2014-05-02 19:00:24 +0200

    Show hierarchy as template and with values filled in side by side

--- src/hiera_explorer/core.clj
+++ src/hiera_explorer/core.clj
@@ -1,6 +1,7 @@
 (ns hiera-explorer.core
   (require [hiccup.page :refer [html5]]
-           [hiera-explorer.yaml :as yaml]))
+           [hiera-explorer.yaml :as yaml]
+           [clojure.string :as str]))
 
 (def config-file "resources/example/hiera.yaml")
 
@@ -20,6 +21,10 @@
    "2aa198"
    "859900"])
 
+(def values
+  {"::group" "dev"
+   "::fqdn" "host1.example.com"})
+
 (def level-styles
   (apply str
          (map-indexed
@@ -27,20 +32,38 @@
             (format ".hier-level-%d { color: #%s; }" index color))
           solarized-colors)))
 
+(defn hierarchy-view [levels]
+  [:ul
+   (map-indexed
+    (fn [index level]
+      [:li {:class (str "hier-level-" index)} level])
+    levels)])
+
+(defn replace-variables [levels value-map]
+  (for [level levels]
+    (str
+     (str/replace level
+                  #"%\{(.*?)\}"
+                  (fn [[_ var-name]]
+                    (get value-map var-name "[not set]")))
+     ".yaml")))
+
 (defn web [request]
-  {:status 200
-   :body
-   (html5
-    [:head
-     [:link
-      {:rel "stylesheet"
-       :href "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"}]
-     [:style level-styles]]
-    [:body
-     navbar
-     [:div.container
-      [:ul
-       (map-indexed
-        (fn [index level]
-          [:li {:class (str "hier-level-" index)} level])
-        (yaml/hierarchy (yaml/load-config config-file)))]]])})
+  (let [conf (yaml/load-config config-file)
+        levels (yaml/hierarchy conf)]
+    {:status 200
+     :body
+     (html5
+      [:head
+       [:link
+        {:rel "stylesheet"
+         :href "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"}]
+       [:style level-styles]]
+      [:body
+       navbar
+       [:div.container
+        [:div.row
+         [:div.col-md-6
+          (hierarchy-view levels)]
+         [:div.col-md-6
+          (hierarchy-view (replace-variables levels values))]]]])}))
