ed188351e7ea6a9f32aca0ce110fd8d4ed850a5a
Christian Berg <berg.christian@gmail.com>
2014-05-05 18:00:04 +0200

    Implement variable expansion in yaml namespace, add tests.

--- src/hiera_explorer/yaml.clj
+++ src/hiera_explorer/yaml.clj
@@ -1,5 +1,6 @@
 (ns hiera-explorer.yaml
-  (:require [clj-yaml.core :as yaml]))
+  (:require [clj-yaml.core :as yaml]
+            [clojure.string :as str]))
 
 (defn load-config [config-file]
   (yaml/parse-string
@@ -7,6 +8,21 @@
 
 (defn hierarchy [config]
   (config (keyword ":hierarchy")))
+
+(defn expand-variables [string value-map]
+  "Replaces patterns of the form %{variable-name} with values from the a map
+
+If a pattern %{variable-name} exists in the string and their either is
+no key of the same name in the map or the value for the key is nil or
+the empty string, do not replace the pattern."
+  (reduce (fn [string [variable-name value]]
+            (if (or (nil? value) (= value ""))
+              string
+              (str/replace string
+                           (str "%{" variable-name "}")
+                           (str value))))
+          string
+          value-map))
 
 (defn scope-vars [config]
   (into #{}
--- test/hiera_explorer/yaml_test.clj
+++ test/hiera_explorer/yaml_test.clj
@@ -4,8 +4,25 @@
 
 (def config-file "resources/example/hiera.yaml")
 
+(deftest variable-expansion
+  (testing "replace all variables in string"
+    (is (= (expand-variables "%{foo}/%{bar}" {"foo" "one" "bar" "two"})
+           "one/two")))
+  (testing "do not replace variables that are not in value-map"
+    (is (= (expand-variables "%{foo}/%{unknown}" {"foo" "one"})
+           "one/%{unknown}")))
+  (testing "do not replace variables that are set to the empty string"
+    (is (= (expand-variables "%{foo}/%{unset}" {"foo" "one" "unset" ""})
+           "one/%{unset}")))
+  (testing "do not replace variables that are set to nil"
+    (is (= (expand-variables "%{foo}/%{unset}" {"foo" "one" "unset" nil})
+           "one/%{unset}")))
+  (testing "do replace variables that are set to false"
+    (is (= (expand-variables "%{foo}/%{bar}" {"foo" "one" "bar" false})
+           "one/false"))))
+
 (deftest config
-  (let  [conf (load-config config-file)]
+  (let [conf (load-config config-file)]
     (testing "load hiera.yaml config file"
       (is (contains? conf (keyword ":backends")))
       (is (contains? conf (keyword ":yaml")))
@@ -14,7 +31,7 @@
       (is (= (first (hierarchy conf))
              "%{::fqdn}"))
       (is (= (last (hierarchy conf))
-             "common")))
+             "global")))
     (testing "finds the scope variables"
       (is (= (scope-vars conf)
-             #{"::fqdn" "::vertical" "::group" "::server_type"})))))
+             #{"::fqdn" "::foo" "::bar"})))))
