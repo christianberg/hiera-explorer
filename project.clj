e1a30dfd44b5d6866da901defc31fbb8f187f509
Christian Berg <berg.christian@gmail.com>
2015-11-24 10:50:41 +0100

    Update dependency: clj-yaml 0.5.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hiera-explorer "0.2.0-SNAPSHOT"
+(defproject hiera-explorer "0.2.1"
   :description "A web app to visualize Hiera configuration data"
   :url "http://github.com/christianberg/hiera-explorer"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring "1.4.0"]
                  [hiccup "1.0.5"]
-                 [circleci/clj-yaml "0.5.4"]
+                 [circleci/clj-yaml "0.5.5"]
                  [me.raynes/fs "1.4.6"]
                  [prone "0.8.2"]]
   :plugins [[lein-ring "0.8.10"]]
