792fa0e70c9dbc268714b58752a6fabcc844419e
Christian Berg <berg.christian@gmail.com>
2016-06-15 09:54:04 +0200

    Update dependency: ring 1.5.0
    Also update the lein-ring plugin to version 0.9.7.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.4.0"]
+                 [ring "1.5.0"]
                  [hiccup "1.0.5"]
                  [circleci/clj-yaml "0.5.5"]
                  [me.raynes/fs "1.4.6"]
                  [prone "1.1.1"]]
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.9.7"]]
   :main hiera-explorer.main
   :aot :all
   :ring {:handler hiera-explorer.core/web})
